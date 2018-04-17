e41c7818cc7e739d23f8165533a616806dfa420f
Kristof Farkas Pall <kristofarkas@gmail.com>
2018-04-17 12:38:10 +0000

    Read in all mutations for run script

--- experiments/all-muts/esmacs-nilotinib.py
+++ experiments/all-muts/esmacs-nilotinib.py
@@ -1,11 +1,16 @@
 from radical.htbac import Esmacs, Runner
 
-muts = ['nilotinib-e255k']
+systems = list()
+
+with open('all-muts.dat') as f:
+    for line in f.readlines():
+        systems.append(line.strip())
 
 ht = Runner()
-esm = Esmacs(number_of_replicas=25, systems=muts, rootdir='../../models', full=True, cores=16, cutoff=10, water_model='tip4')
+esm = Esmacs(number_of_replicas=25, systems=systems, rootdir='../../models', full=True, cores=16, cutoff=10, water_model='tip4')
 
 ht.add_protocol(esm)
 
 ht.rabbitmq_config(hostname='openshift-node1.ccs.ornl.gov', port=30673)
 ht.run(walltime=720)
+
